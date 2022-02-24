468b7d6bb3b69191e3b1fcde0c49d9a2f13679da
Wazir-h <k_aggarwal@ee.iitr.ac.in>
2022-02-24 11:07:32 +0530

    Basic Assignment

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,7 +1,36 @@
 import 'package:flutter/material.dart';
 
-void main()
-{
-  runApp(const MaterialApp(
-    ));
+void main() {
+  var todon;
+
+  final todoncontroller = new TextEditingController();
+
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey,
+        body: SafeArea(
+          child: Row(children: [
+            TextField(
+              controller: todoncontroller,
+              decoration: InputDecoration(
+                hintText: "Add Task",
+                border: OutlineInputBorder(),
+                suffixIcon: IconButton(
+                    icon: Icon(Icons.double_arrow_outlined),
+                    onPressed: () {
+                      setState() {
+                        todon = todon + "\n" + todoncontroller.text;
+                      }
+                    }),
+              ),
+            ),
+            Container(height: 16),
+            Text(todon),
+            Container(height: 16),
+          ]),
+        ),
+      ),
+    ),
+  );
 }
