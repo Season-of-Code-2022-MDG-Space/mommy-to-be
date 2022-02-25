29f56a168c2b64deceba465df02641391abf6d40
Wazir-h <k_aggarwal@ee.iitr.ac.in>
2022-02-25 14:55:26 +0530

    Correction1

--- mommy_to_be/lib/constants.dart
+++ mommy_to_be/lib/constants.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/material.dart';
 
-const kPrimaryColor = Color.fromARGB(206, 106, 87, 212);
+const kPrimaryColor = Color.fromARGB(158, 64, 41, 197);
 const kPrimaryLightColor = Color.fromARGB(255, 255, 255, 255);
